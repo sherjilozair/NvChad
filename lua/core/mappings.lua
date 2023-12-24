b004b9c7db6bf58a8cc18c46b9386a0aaf4d3759
Sherjil Ozair <sherjilozair@gmail.com>
2023-12-23 20:56:29 -0800

    Disable arrow keys

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -4,6 +4,12 @@ local M = {}
 
 M.general = {
   i = {
+    -- Disable arrow keys
+    ["<Up>"] = {"<NOP>", "Does nothing", opts = {noremap = true, silent = true}},
+    ["<Down>"] = {"<NOP>", "Does nothing", opts = {noremap = true, silent = true}},
+    ["<Left>"] = {"<NOP>", "Does nothing", opts = {noremap = true, silent = true}},
+    ["<Right>"] = {"<NOP>", "Does nothing", opts = {noremap = true, silent = true}},
+
     -- go to normal mode
     ["jk"] = {"<Esc>", "Go to normal mode"},
 
@@ -19,6 +25,12 @@ M.general = {
   },
 
   n = {
+    -- Disable arrow keys
+    ["<Up>"] = {"<NOP>", "Does nothing", opts = {noremap = true, silent = true}},
+    ["<Down>"] = {"<NOP>", "Does nothing", opts = {noremap = true, silent = true}},
+    ["<Left>"] = {"<NOP>", "Does nothing", opts = {noremap = true, silent = true}},
+    ["<Right>"] = {"<NOP>", "Does nothing", opts = {noremap = true, silent = true}},
+
     ["<Esc>"] = { "<cmd> noh <CR>", "Clear highlights" },
     -- switch between windows
     ["<C-h>"] = { "<C-w>h", "Window left" },
@@ -35,15 +47,6 @@ M.general = {
     -- line numbers
     ["<leader>n"] = { "<cmd> set nu! <CR>", "Toggle line number" },
     ["<leader>rn"] = { "<cmd> set rnu! <CR>", "Toggle relative number" },
-
-    -- Allow moving the cursor through wrapped lines with j, k, <Up> and <Down>
-    -- http://www.reddit.com/r/vim/comments/2k4cbr/problem_with_gj_and_gk/
-    -- empty mode is same as using <cmd> :map
-    -- also don't use g[j|k] when in operator pending mode, so it doesn't alter d, y or c behaviour
-    ["j"] = { 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"', "Move down", opts = { expr = true } },
-    ["k"] = { 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', "Move up", opts = { expr = true } },
-    ["<Up>"] = { 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', "Move up", opts = { expr = true } },
-    ["<Down>"] = { 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"', "Move down", opts = { expr = true } },
 
     -- new buffer
     ["<leader>b"] = { "<cmd> enew <CR>", "New buffer" },
